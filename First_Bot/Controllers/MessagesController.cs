d2df3bdcf34e18164bc90dfeec1b98a90a0909ab
James Runham <james.runham@reedonline.co.uk>
2016-10-27 17:03:21 +0100

    gre

--- First_Bot/Controllers/MessagesController.cs
+++ First_Bot/Controllers/MessagesController.cs
@@ -34,12 +34,11 @@ namespace First_Bot
 
                         await SetConversationData(incomingMessage, "keywords", keywords);
 
-                        reply = incomingMessage.CreateReply("Is your location Leeds?");
-                        await SetConversationData(incomingMessage, "location", "leeds");
+                        reply = incomingMessage.CreateReply("What is your location?");
                     }
-                    else if (incomingMessage.Text.ToLowerInvariant().Contains("no it's"))
+                    else if (incomingMessage.Text.ToLowerInvariant().Contains("my location is"))
                     {
-                        var location = incomingMessage.Text.ToLowerInvariant().Replace("no it's", "");
+                        var location = incomingMessage.Text.ToLowerInvariant().Replace("my location is", "");
 
                         await SetConversationData(incomingMessage, "location", location);
 
